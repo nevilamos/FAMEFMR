bf204da2fe4896aacd8f19ac9d0340cb0b5f3b5b
Nevil Amos <nevil.amos@gmail.com>
2020-09-08 22:46:48 +1000

    Minor tweaks to makeSpYearSumm2 function to deal with reading HDMValues from FileMatrix rather than raster or matrix as in FAMEv1 and 1.1
    makeSpYearSumm2 now in ECOresFunctionsFMRv2.r file, fucnions in this file that are not being used in FAME V2 have been moved to bottom of file after a comment to that effect.

---
+++ makeHDM_Value_Matrices.R
@@ -0,0 +1,32 @@
+#This function  compiles vectors of raster values  stored in ./HDMS/75m or ./HDMS/225m (75 m or 225 m versions), to
+#filematrix files on disk - these are large, but faster to read the data from
+#than reading directly from raster or an .rdata file 
+
+# It takes a fair while to run - but only needs to be  run if the rasters ar
+# updated, or new rasters added.  The rasters are  values are stored as a single
+# column vector per raster with the character form of the VBA TAXON_ID as the
+# column name - this is later used to retrieve the values in species
+# calculations currently these rasters contain binary values - and act as a
+# species  mask on the abundace values. The rasters could be updated to
+# continuous or thresholded continuous values - if this is done change the
+# commented line below from the one with type="logical" to type = "double"  THIS
+# will result in greated disk storage space requirments for the filematrix
+# files, and memory for cacluations
+
+
+#individual new rasters' values can be appended easily to the fm objects, or
+#individual species rasters replaced by using the colname - this is faster than
+#repeating the whole process ( which takes >1 hour for 577 75m rasters)
+
+for (j in c(75,225)){
+  myTiffs<-dir(file.path(HDMDir,paste0(j,"m")),pattern=".tif",recursive=T, full.names=T)
+  myCols<-length(myTiffs)
+  myRows<-ncell(raster(myTiffs[1]))
+  
+  fm<-fm.create(filenamebase = file.path("HDMS",(paste0("HDMVals",j))),nrow=myRows,ncol=myCols,type="logical")
+  for (i in 1:myCols){
+    fm[,i]<-values(raster(myTiffs[i]))
+    print(i)
+  }
+  colnames(fm)<-as.character(get_Spp_No(myTiffs))
+}
