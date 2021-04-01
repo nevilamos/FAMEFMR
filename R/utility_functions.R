97988233aaab261f5fa24ce746c50d366f3056d2
nevilamos <nevil.amos@gmail.com>
2021-04-01 23:48:28 +1100

    Added geomean fucntion to utility fuctions - copied from https://cran.r-project.org/web/packages/EnvStats/index.html

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -109,8 +109,28 @@ unlistPivot_wider <- function(df){
 }
 
 
-
-
+#' Calculate Geometric mean
+#' @details Calculates geometric mean of a vector of positive numeric values
+#' copied from R package EnvStats https://cran.r-project.org/web/packages/EnvStats/index.html
+#' @param x numeric vector
+#'
+#' @return numeric or NA if values are not all > 0
+geoMean<-function (x, na.rm = FALSE)
+{
+  if (!is.vector(x, mode = "numeric") || is.factor(x))
+    stop("'x' must be a numeric vector")
+  wna <- which.na(x)
+  if (length(wna)) {
+    if (na.rm)
+      x <- x[-wna]
+    else return(NA)
+  }
+  if (any(x <= 0)) {
+    warning("Non-positive values in 'x'")
+    return(NA)
+  }
+  else return(exp(mean(log(x))))
+}
 
 
 
