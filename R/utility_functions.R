0677fcd24282201a15dd10ac0334a10daee40eb5
nevilamos <nevil.amos@gmail.com>
2021-04-07 12:36:03 +1000

    changed geoMean function to one not requireng addtional packages

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -111,25 +111,21 @@ unlistPivot_wider <- function(df){
 
 #' Calculate Geometric mean
 #' @details Calculates geometric mean of a vector of positive numeric values
-#' copied from R package EnvStats https://cran.r-project.org/web/packages/EnvStats/index.html
+
 #' @param x numeric vector
 #'
 #' @return numeric or NA if values are not all > 0
-geoMean<-function (x, na.rm = FALSE)
-{
-  if (!is.vector(x, mode = "numeric") || is.factor(x))
-    stop("'x' must be a numeric vector")
-  wna <- which.na(x)
-  if (length(wna)) {
-    if (na.rm)
-      x <- x[-wna]
-    else return(NA)
-  }
-  if (any(x <= 0)) {
-    warning("Non-positive values in 'x'")
-    return(NA)
-  }
-  else return(exp(mean(log(x))))
+geoMean<-function (x, na.rm = FALSE){
+  if(any(is.na(x))){
+    y = NA
+    warning("vector contains NA values")
+    } else if(any(x<=0)){
+      y=NA
+      warning("Not all x are positive values")
+    } else {
+        y=prod(x)^(1/length(x))
+        }
+  return(y)
 }
 
 
