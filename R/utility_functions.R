57e6948c647e5352298c1c57b1854e803d36fedd
nevilamos <nevil.amos@gmail.com>
2021-04-07 15:27:24 +1000

    corrected missing @ export for fucntions calc_G and geoMean

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -109,25 +109,6 @@ unlistPivot_wider <- function(df){
 }
 
 
-#' Calculate Geometric mean
-#' @details Calculates geometric mean of a vector of positive numeric values
-
-#' @param x numeric vector
-#'
-#'
-#' @return numeric or NA if values are not all > 0
-geoMean<-function (x){
-  if(any(is.na(x))){
-    y = NA
-    warning("vector contains NA values")
-    } else if(any(x<=0)){
-      y=NA
-      warning("Not all x are positive values")
-    } else {
-        y=prod(x)^(1/length(x))
-        }
-  return(y)
-}
 
 
 
