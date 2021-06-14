771c1b186b4b2ae8988a88c128d636e0aa26627d
nevilamos <nevil.amos@gmail.com>
2021-06-14 16:09:54 +1000

    added pacakge:: to functions in jfmpSummary()

--- R/jfmpSummary.r
+++ R/jfmpSummary.r
@@ -9,19 +9,18 @@
 #'– Score for each metrics (x4) if JFMP not implemented
 #'- Hectares allocated in each FMZ_ID
 #' @export
-#'
-#' @examples
+
 jfmpSummary<- function(JFMP_DF="JFMPSummary with burned or unburned"){
 
 JFMP_DF = AutomJFMP_DF
   JFMP_Summ<-JFMP_DF%>%
-    ungroup()%>%
-    mutate(FaunaRAImp=if_else(AutoJFMP_State=="BURN",WtSumRA_Burn,WtSumRA_NoBurn))%>%
-    mutate(NeverBBTFI_Imp=if_else(AutoJFMP_State=="BURN",PuAreaHa- (ifelse(is.na(Burn_BBTFI),0,Burn_BBTFI)) - (ifelse(is.na(NoBurn_BBTFI),0,NoBurn_BBTFI)),PuAreaHa- (ifelse(is.na(NoBurn_BBTFI),0,NoBurn_BBTFI))))%>%
-    mutate(LP1_Imp =if_else(AutoJFMP_State=="BURN",LP1_Burn,LP1_NoBurn))%>%
-    mutate(LP2_Imp =if_else(AutoJFMP_State=="BURN",LP2_Burn,LP2_NoBurn))%>%
-    group_by(DISTRICT_N)%>%
-    summarize(AreaHa =sum(PuAreaHa,na.rm=T),
+    dplyr::ungroup()%>%
+    dplyr::mutate(FaunaRAImp=if_else(AutoJFMP_State=="BURN",WtSumRA_Burn,WtSumRA_NoBurn))%>%
+    dplyr::mutate(NeverBBTFI_Imp=if_else(AutoJFMP_State=="BURN",PuAreaHa- (ifelse(is.na(Burn_BBTFI),0,Burn_BBTFI)) - (ifelse(is.na(NoBurn_BBTFI),0,NoBurn_BBTFI)),PuAreaHa- (ifelse(is.na(NoBurn_BBTFI),0,NoBurn_BBTFI))))%>%
+    dplyr::mutate(LP1_Imp =if_else(AutoJFMP_State=="BURN",LP1_Burn,LP1_NoBurn))%>%
+    dplyr::mutate(LP2_Imp =if_else(AutoJFMP_State=="BURN",LP2_Burn,LP2_NoBurn))%>%
+    dplyr::group_by(DISTRICT_N)%>%
+    dplyr::summarize(AreaHa =sum(PuAreaHa,na.rm=T),
               SumFaunaRAImp=sum(FaunaRAImp,na.rm=T),
               SumFaunaRANoJFMP=sum(WtSumRA_NoBurn,na.rm=T),
               SumNeverBBTFI_Imp=sum(NeverBBTFI_Imp,na.rm=T),
@@ -30,24 +29,24 @@ JFMP_DF = AutomJFMP_DF
               SumLP1_NoJFMP=sum(LP1_NoBurn,na.rm=T),
               SumLP2_Imp=sum(LP2_Imp,na.rm=T),
               SumLP2_NoJFMP=sum(LP2_NoBurn,na.rm=T))
-  
+
   AreasBurnedUnburned<-JFMP_DF%>%
-    ungroup()%>%
-    group_by(DISTRICT_N,AutoJFMP_State)%>%
-    summarise(AreaHa=sum(PuAreaHa,na.rm=T))%>%
-    spread(AutoJFMP_State,AreaHa)%>%
-    rename(Burned_ha='BURN',NoBurn_Ha='NO BURN')
-  
+    dplyr::ungroup()%>%
+    dplyr::group_by(DISTRICT_N,AutoJFMP_State)%>%
+    dplyr::summarise(AreaHa=sum(PuAreaHa,na.rm=T))%>%
+    dplyr::spread(AutoJFMP_State,AreaHa)%>%
+    dplyr::rename(Burned_ha='BURN',NoBurn_Ha='NO BURN')
+
   AreasBurnedbyFMZ_CODE<-JFMP_DF%>%
-    ungroup()%>%
-    filter(AutoJFMP_State=='BURN')%>%
-    group_by(DISTRICT_N,X_ZONETYPE)%>%
-    summarise(AreaHa=sum(PuAreaHa,na.rm=T))%>%
-    spread(X_ZONETYPE,AreaHa)
+    dplyr::ungroup()%>%
+    dplyr::filter(AutoJFMP_State=='BURN')%>%
+    dplyr::group_by(DISTRICT_N,FMZ_ID)%>%
+    dplyr::summarise(AreaHa=sum(PuAreaHa,na.rm=T))%>%
+    dplyr::spread(FMZ_ID,AreaHa)
   names(AreasBurnedbyFMZ_CODE)[-1]<-paste("Burned_ha ",names(AreasBurnedbyFMZ_CODE)[-1])
-    
+
   JFMP_Summ<-left_join(AreasBurnedUnburned,JFMP_Summ)
   JFMP_Summ<-left_join(JFMP_Summ,AreasBurnedbyFMZ_CODE)
   return(JFMP_Summ)
-  
+
 }
