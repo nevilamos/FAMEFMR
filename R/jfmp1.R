627838f3aa1da83ff51f27615a6766f287584227
nevilamos <nevil.amos@gmail.com>
2021-07-27 14:45:52 +1000

    removed a heap of apparently duplicated code from JFMP1  which was mseeing up rankings

--- R/jfmp1.R
+++ R/jfmp1.R
@@ -26,9 +26,13 @@ jfmp1 <- function(myPUPath = rv$puPath,
                   myJFMPSeason0 = rv$JFMPSeason0,
                   zoneWt = rv$zoneWt,
                   jfmpMetricWt = rv$jfmpMetricWt)
-  {
+{
+
   #Wrangle the SpYearSummRA grouped on index of all combinations of rasters, plus the TaxonList that includes count of cells in area of interest to get the weighted sum of change all species in area of interest for each PU ------
+
+  #calcuate the SEASON after the end of the JFMP this is the SEASON for which scores are calculated
   JFMPSeason4 = myJFMPSeason0 + 4
+
   PU_WeightedSumRA <- grpSpYearSumm %>%
     dplyr::rename(Index_AllCombs = `myAllCombs$Index_AllCombs`) %>%
     tidyr::pivot_longer(
@@ -36,24 +40,34 @@ jfmp1 <- function(myPUPath = rv$puPath,
       names_to = "SEASON",
       values_to = "sumRA"
     ) %>%
+    #remove all seasons except the one for getting BURN and NO_BURN scores
     dplyr::filter(SEASON %in% c(as.character(JFMPSeason4), "NoBurn")) %>%
     dplyr::mutate(SEASON = ifelse(SEASON == "NoBurn", "NoBurn", "Burn")) %>%
+    #add the PU index values
     dplyr::mutate(PU = myAllCombs$U_AllCombs_TFI$PU[Index_AllCombs]) %>%
+    #group and summarise to get scores for each
     dplyr::group_by(TAXON_ID, PU, SEASON,
                     SEASON = paste0("WtSumRA_", SEASON)) %>%
     dplyr::summarise(sumRA = sum(sumRA)) %>%
+    #join the cellsInArea field from taxon List
     dplyr::mutate(TAXON_ID = as.integer(TAXON_ID)) %>%
     dplyr::left_join(myTaxonList %>%
                        dplyr::select(TAXON_ID, cellsInArea)) %>%
+    #calculate the for weighted sum of RA through division by
+    #number of species cells (amount of species habitat) in the study area
     dplyr::mutate(weightedRA = sumRA / cellsInArea) %>%
+    #sum the weighted sum of Relative abundance for all species in each PU for burned and unburned state
     dplyr::group_by(PU, SEASON) %>%
     dplyr::summarise(WeightedSumRA = sum(weightedRA)) %>%
+    #calcuate the difference between the sum for burned and unburned states to
+    #get the score for impact of burning for the PU value will be larger
+    #where burning has a negative impact. It will be negative where burning increases the abundance of a species ( smaller is good)
     tidyr::pivot_wider(names_from = SEASON, values_from = WeightedSumRA) %>%
-    dplyr::mutate(WtSumRA_Diff = WtSumRA_Burn - WtSumRA_NoBurn)
+    dplyr::mutate(WtSumRA_Diff = WtSumRA_NoBurn - WtSumRA_Burn)
 
   print("calculated PU weighted RA")
 
-  #Data wrangling of BBTFI outputs to extract area BBTFI for each PU, this requires inclusion ONLY of the first time areas are buned below TFI, these are then categorised as "PastBBTFI" for all years up to JFMPSeason0, and then as BBTFI (for the first time) by the JFMP)----
+  #Data wrangling of BBTFI outputs to extract area BBTFI for each PU, this requires inclusion ONLY of the first time areas are burned below TFI, these are then categorised as "PastBBTFI" for all years up to JFMPSeason0, and then as BBTFI (for the first time) by the JFMP)----
   print("doing JFMPBBTFI")
 
 
@@ -67,63 +81,46 @@ jfmp1 <- function(myPUPath = rv$puPath,
 
 
   #read PU shapefile and remove geometry
-    puDF <- sf::st_read(myPUPath)
-    puDF$geometry <- NULL
-    puDF <- puDF %>%
-      dplyr::mutate(LP1_Diff = LP1_Burn - LP1_NoBurn,
-                    LP2_Diff = LP2_Burn - LP2_NoBurn) %>%
-      dplyr::left_join(PU_BBTFI_Summ) %>%
-      dplyr::mutate(BBTFI_Diff = ifelse(is.na(Burn_BBTFI),0,Burn_BBTFI))%>%
-      dplyr::left_join(PU_WeightedSumRA)
+  puDF <- sf::st_read(myPUPath)
+  puDF$geometry <- NULL
+  puDF <- puDF %>%
+    #calculate the scores for LP1 and LP2 Burn minus unburned small
+    #(negative) is good if LP risk is lower when burned the value will be <0
+    dplyr::mutate(LP1_Diff = LP1_Burn - LP1_NoBurn,
+                  LP2_Diff = LP2_Burn - LP2_NoBurn) %>%
+    dplyr::left_join(PU_BBTFI_Summ) %>%
+    dplyr::mutate(BBTFI_Diff = ifelse(is.na(Burn_BBTFI),0,Burn_BBTFI))%>%
+    dplyr::left_join(PU_WeightedSumRA) %>%
 
-# ranking process
-    puDF<-puDF%>%
-      #By WHOLE AREA
-      dplyr::mutate(WtSumRA_DiffStd=scales::rescale(WtSumRA_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
-      dplyr::mutate(BBTFI_DiffStd=scales::rescale(BBTFI_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
-      dplyr::mutate(LP1Std=scales::rescale(LP1_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
-      dplyr::mutate(LP2Std=scales::rescale(LP2_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
-      dplyr::mutate(WtSumRA_RankREGION=dplyr::dense_rank(WtSumRA_Diff))%>%#ranking by Diff
-      dplyr::mutate(BBTFI_RankREGION=dplyr::dense_rank(BBTFI_Diff))%>%#ranking by Diff
-      dplyr::mutate(LP1_RankREGION=dplyr::dense_rank(LP1_Diff))%>%#ranking by Diff
-      dplyr::mutate(LP2_RankREGION=dplyr::dense_rank(LP2_Diff))%>%#ranking by Diff
-      #By DISTRICT
-      dplyr::group_by(DISTRICT_N)%>%
-      dplyr::mutate(WtSumRA_RankDISTRICT=dplyr::dense_rank(WtSumRA_Diff))%>%#ranking by Diff within District
-      dplyr::mutate(BBTFI_RankDISTRICT=dplyr::dense_rank(BBTFI_Diff))%>%#ranking by Diff within District
-      dplyr::mutate(LP1_RankDISTRICT=dplyr::dense_rank(LP1_Diff))%>%#ranking by Diff within District
-      dplyr::mutate(LP2_RankDISTRICT=dplyr::dense_rank(LP2_Diff))%>%#ranking by Diff within District
-      #By DISTRICT and ZONE
-      dplyr::group_by(DISTRICT_N,FMZ_CODE)%>%
-      dplyr::mutate(WtSumRA_RankDISTRICT_FMZ=dplyr::dense_rank(WtSumRA_Diff))%>%#ranking by Diff within District and FMZ
-      dplyr::mutate(BBTFI_RankDISTRICT_FMZ=dplyr::dense_rank(BBTFI_Diff))%>%#ranking by Diff within District and FMZ
-      dplyr::mutate(LP1_RankDISTRICT_FMZ=dplyr::dense_rank(LP1_Diff))%>%#ranking by Diff within District and FMZ
-      dplyr::mutate(LP2_RankDISTRICT_FMZ=dplyr::dense_rank(LP2_Diff))#ranking by Diff within District and FMZ
+    dplyr::mutate(WtSumRA_Diff = WtSumRA_NoBurn - WtSumRA_Burn)
 
-    #Scale the scores and then calcuate weighted aggreagate score for JFMP
+  # ranking process
   puDF<-puDF%>%
+    #By WHOLE AREA
     dplyr::mutate(WtSumRA_DiffStd=scales::rescale(WtSumRA_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
     dplyr::mutate(BBTFI_DiffStd=scales::rescale(BBTFI_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
     dplyr::mutate(LP1Std=scales::rescale(LP1_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
     dplyr::mutate(LP2Std=scales::rescale(LP2_Diff,na.rm=T))%>% #standardised the Diffs ( scale 0-1) so that it makes sense to add them
-    dplyr::mutate(WtSumRA_RankREGION=dplyr::dense_rank(WtSumRA_Diff))%>%#ranking by Diff
-    dplyr::mutate(BBTFI_RankREGION=dplyr::dense_rank(BBTFI_Diff))%>%#ranking by Diff
-    dplyr::mutate(LP1_RankREGION=dplyr::dense_rank(LP1_Diff))%>%#ranking by Diff
-    dplyr::mutate(LP2_RankREGION=dplyr::dense_rank(LP2_Diff))%>%#ranking by Diff
+    dplyr::mutate(WtSumRA_RankREGION=dense_rank(WtSumRA_Diff))%>%#ranking by Diff
+    dplyr::mutate(BBTFI_RankREGION=dense_rank(BBTFI_Diff))%>%#ranking by Diff
+    dplyr::mutate(LP1_RankREGION=dense_rank(LP1_Diff))%>%#ranking by Diff
+    dplyr::mutate(LP2_RankREGION=dense_rank(LP2_Diff))%>%#ranking by Diff
+    #By DISTRICT
     dplyr::group_by(DISTRICT_N)%>%
-    dplyr::mutate(WtSumRA_RankDISTRICT=dplyr::dense_rank(WtSumRA_Diff))%>%#ranking by Diff within District
-    dplyr::mutate(BBTFI_RankDISTRICT=dplyr::dense_rank(BBTFI_Diff))%>%#ranking by Diff within District
-    dplyr::mutate(LP1_RankDISTRICT=dplyr::dense_rank(LP1_Diff))%>%#ranking by Diff within District
-    dplyr::mutate(LP2_RankDISTRICT=dplyr::dense_rank(LP2_Diff))%>%#ranking by Diff within District
+    dplyr::mutate(WtSumRA_RankDISTRICT=dense_rank(WtSumRA_Diff))%>%#ranking by Diff within District
+    dplyr::mutate(BBTFI_RankDISTRICT=dense_rank(BBTFI_Diff))%>%#ranking by Diff within District
+    dplyr::mutate(LP1_RankDISTRICT=dense_rank(LP1_Diff))%>%#ranking by Diff within District
+    dplyr::mutate(LP2_RankDISTRICT=dense_rank(LP2_Diff))%>%#ranking by Diff within District
+    #By DISTRICT and ZONE
     dplyr::group_by(DISTRICT_N,FMZ_CODE)%>%
-    dplyr::mutate(WtSumRA_RankDISTRICT_FMZ=dplyr::dense_rank(WtSumRA_Diff))%>%#ranking by Diff within District and FMZ
-    dplyr::mutate(BBTFI_RankDISTRICT_FMZ=dplyr::dense_rank(BBTFI_Diff))%>%#ranking by Diff within District and FMZ
-    dplyr::mutate(LP1_RankDISTRICT_FMZ=dplyr::dense_rank(LP1_Diff))%>%#ranking by Diff within District and FMZ
-    dplyr::mutate(LP2_RankDISTRICT_FMZ=dplyr::dense_rank(LP2_Diff))%>% #ranking by Diff within District and FMZ
-    dplyr::left_join(merge(zoneWt,jfmpMetricWt))%>% # calculate scaled scores with weightings to get overall score
+    dplyr::mutate(WtSumRA_RankDISTRICT_FMZ=dense_rank(WtSumRA_Diff))%>%#ranking by Diff within District and FMZ
+    dplyr::mutate(BBTFI_RankDISTRICT_FMZ=dense_rank(BBTFI_Diff))%>%#ranking by Diff within District and FMZ
+    dplyr::mutate(LP1_RankDISTRICT_FMZ=dense_rank(LP1_Diff))%>%#ranking by Diff within District and FMZ
+    dplyr::mutate(LP2_RankDISTRICT_FMZ=dense_rank(LP2_Diff))#ranking by Diff within District and FMZ
+
+  #Scale the scores and then calculate weighted aggregate score for JFMP
+  puDF<-puDF%>%dplyr::left_join(merge(zoneWt,jfmpMetricWt))%>% # calculate scaled scores with weightings to get overall score
     dplyr::mutate(DiffSum = (LP1Std * LP1Wt + LP2Std * LP2Wt) * LPwt + (WtSumRA_DiffStd * FaunaWt + BBTFI_DiffStd *FloraWt) * BDwt)
 
   print("finished jfmp1")
-
-  return(puDF)
-}
+  }
