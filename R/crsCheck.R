6be6cc35b258333cce87a18978de82732cc2a46b
Simonestevo <simonestevenson@gmail.com>
2026-02-03 15:10:04 +1100

    added epsg as an argument, to the documentation, and added a helper function to convert epsg to numeric if it is entered in another format

--- R/crsCheck.R
+++ R/crsCheck.R
@@ -14,12 +14,14 @@
 #' @param validFIRETYPE vector of valid names in the input FIRETYPE column in
 #'   the input fire history dataset(s), if the column contains NA or values not
 #'   on this list an error will occur
+#' @param epsg a string denoting the user's preferred coordinate reference system.
+#' Should be in the format "epsg:3111"
 #' @return correctly formatted Fire History Polygon dataset as sf for use in
 #' fhProcess1()
 #' @export
 #'
 
-crsCheck <- function(inFH=inFH, inFHLayer =inFHLayer, validFIRETYPE =validFIRETYPE, epsg = NULL){
+crsCheck <- function(inFH=inFH, inFHLayer =inFHLayer, validFIRETYPE = validFIRETYPE, epsg = NULL){
 
   # Check that the user has supplied an epsg code, if they haven't stop the function
 
@@ -27,10 +29,40 @@ crsCheck <- function(inFH=inFH, inFHLayer =inFHLayer, validFIRETYPE =validFIRETY
     stop("Error: no epsg code (coordinate reference system) has been supplied by the user")
   }
 
-  # If they have, convert the character string to just the numeric code so we have both
-  epsg_numeric <- suppressWarnings(
-    as.numeric(substr(epsg, nchar(epsg) - 3, nchar(epsg)))
-  )
+  # Function to convert epsg numeric in case it is supplied in a different format
+  make_epsg_numeric <- function(epsg) {
+
+  if(is.numeric(epsg)) {
+
+    epsg_numeric <- epsg
+
+    return(epsg_numeric)
+
+  } else if (is.character(epsg)) {
+
+    # If they have, convert the character string to just the numeric code so we have both
+    epsg_numeric <- suppressWarnings(
+      as.numeric(substr(epsg, nchar(epsg) - 3, nchar(epsg)))
+    )
+
+    return(epsg_numeric)
+
+    } else if(class(epsg) == "crs") {
+
+    epsg_numeric <- epsg$epsg
+
+    return(epsg_numeric)
+
+    } else {
+
+      stop("epsg must be numeric, character, or an sf::crs object")
+
+    }
+
+  }
+
+  # Apply the standardisation function
+  epsg_numeric <- make_epsg_numeric(epsg)
 
   # Basic checks on input Fire History file----
   if("character" %in% (class(inFH))){
@@ -64,12 +96,16 @@ crsCheck <- function(inFH=inFH, inFHLayer =inFHLayer, validFIRETYPE =validFIRETY
   if ((!"FIRETYPE" %in% mySFNames))     {stop("inFH file does not contain field 'FIRETYPE'")}
   if (notAllIn(x = mySF$FIRETYPE, v = validFIRETYPE))  {stop("inFH shapefile has missing or invalid values in field 'FIRETYPE'")}
 
-  inEPSG<-sf::st_crs(mySF)$epsg
+  inEPSG <- sf::st_crs(mySF)$epsg
+
   if (inEPSG == epsg_numeric){ #3111 numeric
 
-  }else if(is.numeric(inEPSG)){
-    mySF<-sf::st_transform(mySF,crs = epsg) #"epsg:3111" character
-  } else{
+  } else if (is.numeric(inEPSG)){
+
+    mySF <- sf::st_transform(mySF,crs = epsg_numeric)
+
+  } else {
+
     stop("input Fire History projection is not adequately defined. Ideally should be epsg:3111 (VicGrid94)")
   }
   sf::st_geometry(mySF)<-"geometry" # ensures that name of geometry column is always "geometry"
